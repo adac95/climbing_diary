b5af2e43b41f042ef9ce21651707f0264bfd627f
Andre del Alcazar Caceres <adac9525@gmail.com>
2023-12-12 16:46:37 -0500

    migrando de supabase helpers a supabase/ssr

--- src/app/topos/fetchData.js
+++ src/app/topos/fetchData.js
@@ -1,27 +1,45 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+'use server'
+import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
+const cookieStore = cookies();
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get(name) {
+          return cookieStore.get(name)?.value;
+        },
+      },
+    }
+  );
 
 export async function getAllRegions() {
-    const supabase = createServerComponentClient({cookies})
-    const {data} = await supabase.from('region').select()
-    return data
+  // const supabase = createServerComponentClient({cookies})
+  
+  const { data } = await supabase.from("region").select();
+  return data;
 }
 
 export async function getAllPlaces() {
-    const supabase = createServerComponentClient({cookies})
-    const {data} = await supabase.from('place').select("*, approach(information,public_transport,private_transport),camping(is_available,price),lodge(is_available,price),region_id(id,name)")
-    return data
+//   const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase
+    .from("place")
+    .select(
+      "*, approach(information,public_transport,private_transport),camping(is_available,price),lodge(is_available,price),region_id(id,name)"
+    );
+  return data;
 }
 
 export async function getAllSectos() {
-    const supabase = createServerComponentClient({cookies})
-    const {data} = await supabase.from('sector').select()
-    return data
+//   const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.from("sector").select();
+  return data;
 }
 
 export async function getAllRoutes() {
-    const supabase = createServerComponentClient({cookies})
-    const {data} = await supabase.from('route').select()
-    return data
+//   const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.from("route").select();
+  return data;
 }
--- src/components/PokedexNewRouteModal/index.js
+++ src/components/PokedexNewRouteModal/index.js
@@ -1,37 +1,58 @@
+'use client'
+
+import { useRef } from "react";
+import styles from "./PokedexNewRouteModal.module.css";
+
 export default function PokedexNewRouteModal() {
+
+ const form = useRef()
+  const handlesubmit = function (e) {
+
+      e.preventDefault();
+    const formData = new FormData(form.current)
+
+    console.log({formData}, form.current);
+  };
+
   return (
-    <section>
+    <form ref={form} onSubmit={(e) => handlesubmit(e)} className={styles.container}>
       <h2>Creando nueva ruta en tu pokedex</h2>
-      <label>
+      <label className={styles.label} id='name' htmlFor='name'>
         Nombre de la ruta
-        <input type='text' />
+        <input className={styles.input} type='text' id='name' name='name' />
       </label>
-      <label>
+      <label className={styles.label}>
         Estilo
-        <select>
+        <select className={styles.input}>
           <option>deportiva</option>
           <option>boulder</option>
           <option>tradicional</option>
-        </select>{" "}
+        </select>
       </label>
-      <label>
+      <label className={styles.label}>
         grado
-        <input type='text' />
+        <input className={styles.input} type='text' />
       </label>
-      <fieldset>
-        <legend>Salió?</legend>
-        <input id='encadenado' type='radio' name='a' value='encadenado' />
-        <label htmlFor='encadenado'>Encadenado </label>
-        <input id='proyecto' type='radio' name='a' value='proyecto' />
-        <label htmlFor='proyecto'>Proyecto </label>
+      <fieldset className={styles.fieldset}>
+        <legend className={styles.legend} >Salió?</legend>
+        <label className={styles.label} htmlFor='encadenado'>
+          <input id='encadenado' type='radio' name='a' value='encadenado' />
+          Encadenado{" "}
+        </label>
+        <label className={styles.label} htmlFor='proyecto'>
+          <input id='proyecto' type='radio' name='a' value='proyecto' />
+          Proyecto{" "}
+        </label>
       </fieldset>
-      <label>
-        Nro pegues <input />
+      <label className={styles.label}>
+        Nro pegues <input className={styles.input} />
       </label>
-      <label>Comentarios
-        <textarea />
+      <label className={styles.label}>
+        Comentarios
+        <textarea className={styles.input} rows='6' />
       </label>
-      <button>Subir archivos</button>
-    </section>
+       <label className={styles.label}> Subir archivos <input type="file" className={styles.submitBtn}  /></label>
+      <button className={styles.submitBtn} type="submit">Agregar al pokedex</button>
+    </form>
   );
 }
