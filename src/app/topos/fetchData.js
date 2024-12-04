794f6503c8791b5ee0999450b29f1a2253d6d13f
Andre del Alcazar Caceres <adac9525@gmail.com>
2024-12-04 12:17:38 -0500

    implementando middlewares para la session de supabase en server y client components

--- src/app/topos/fetchData.js
+++ src/app/topos/fetchData.js
@@ -1,29 +1,13 @@
-'use server'
-import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
-
-const cookieStore = cookies();
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    {
-      cookies: {
-        get(name) {
-          return cookieStore.get(name)?.value;
-        },
-      },
-    }
-  );
+import { createClient } from "@utils/supabase/server";
+const supabase = await createClient()
 
 export async function getAllRegions() {
-  // const supabase = createServerComponentClient({cookies})
-  
   const { data } = await supabase.from("region").select();
   return data;
 }
 
 export async function getAllPlaces() {
-//   const supabase = createServerComponentClient({ cookies });
+
   const { data } = await supabase
     .from("place")
     .select(
@@ -33,13 +17,13 @@ export async function getAllPlaces() {
 }
 
 export async function getAllSectos() {
-//   const supabase = createServerComponentClient({ cookies });
+
   const { data } = await supabase.from("sector").select();
   return data;
 }
 
 export async function getAllRoutes() {
-//   const supabase = createServerComponentClient({ cookies });
+
   const { data } = await supabase.from("route").select();
   return data;
 }
