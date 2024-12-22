13e120bd53ce4eb13b71bc564be0e04628222afe
Andre del Alcazar Caceres <adac9525@gmail.com>
2024-12-22 15:28:00 -0500

    Añadiendo comprobaciones de seguridad para evitar errores al acceder a propiedades de objetos en PlaceToRender.

--- src/app/topos/components/PlaceToRender/index.js
+++ src/app/topos/components/PlaceToRender/index.js
@@ -1,7 +1,6 @@
 import styles from "./PlaceToRender.module.css";
 
 export function PlaceToRender({ place }) {
-  console.log(place.lodge[0].price);
   return (
     <div className={styles.container} style={{ whiteSpace: "pre-line" }}>
       <h2 className={styles.tittle}>{place?.name}</h2>
@@ -11,11 +10,11 @@ export function PlaceToRender({ place }) {
       </span>
       <span>
         <h3>¿Cómo llegar?</h3>
-        <p>{place.approach[0].information}</p>
+        <p>{place.approach[0]?.information}</p>
         <h4>Transporte público:</h4>
-        <p>{place.approach[0]["public_transport"]}</p>
+        <p>{place.approach[0]?.["public_transport"]}</p>
         <h4>Transporte privado:</h4>
-        <p>{place.approach[0]["private_transport"]}</p>
+        <p>{place.approach[0]?.["private_transport"]}</p>
       </span>
       <span>
         <h3>Precios</h3>
@@ -23,10 +22,10 @@ export function PlaceToRender({ place }) {
       </span>
       <span>
         <h3>Zona de camping</h3>
-        {place.camping[0]["is_available"] ? (
+        {place.camping[0]?.["is_available"] ? (
           <>
-            <p>{place.camping[0].information}</p>
-            <p>{place.camping[0].price}</p>
+            <p>{place.camping[0]?.information}</p>
+            <p>{place.camping[0]?.price}</p>
           </>
         ) : (
           <p>No disponible</p>
@@ -34,10 +33,10 @@ export function PlaceToRender({ place }) {
       </span>
       <span>
         <h3>Lodge/Hospedaje/Refugio</h3>
-        {place.lodge[0]["is_available"] ? (
+        {place.lodge[0]?.["is_available"] ? (
           <>
-            <p>{place.lodge[0].information}</p>
-            <p>{place.lodge[0].price}</p>
+            <p>{place.lodge[0]?.information}</p>
+            <p>{place.lodge[0]?.price}</p>
           </>
         ) : (
           <p>No disponible</p>
